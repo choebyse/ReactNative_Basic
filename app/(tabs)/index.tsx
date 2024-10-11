adca2f7be383690fca75f800a9c251d34d79dcc6
CHOE BYEONGSEON <choe.byse@gmail.com>
2024-10-11 11:13:20 +0900

    todos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,9 +15,22 @@ import { theme } from "./colors";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload: string) => setText(payload);
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work, working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -42,7 +55,9 @@ export default function App() {
       </View>
       <View>
         <TextInput
+          onSubmitEditing={addToDo}
           onChangeText={onChangeText}
+          returnKeyType="done"
           value={text}
           placeholder={working ? "Add a To Do" : "Where do you want to go"}
           style={styles.input}
