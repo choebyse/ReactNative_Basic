a6ce243fc5a9c123e6786e7007645a058db65eec
CHOE BYEONGSEON <choe.byse@gmail.com>
2024-10-17 00:31:16 +0900

    saveworking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -19,20 +19,41 @@ interface ToDo {
 }
 
 const STORGE_KEY = "@toDos";
+const WORKING_KEY = "@working";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState<{ [key: string]: ToDo }>({});
+
+  //App実行した時、状態を呼び出す。
   useEffect(() => {
+    loadWorking();
     loadToDos();
   }, []);
-  const travel = () => setWorking(false);
-  const work = () => setWorking(true);
+
+  const travel = () => {
+    setWorking(false);
+    saveWorking(false);
+  };
+
+  const work = () => {
+    setWorking(true);
+    saveWorking(true);
+  };
+
   const onChangeText = (payload: string) => setText(payload);
 
   const saveToDos = async (toSave: {}) => {
     await AsyncStorage.setItem(STORGE_KEY, JSON.stringify(toSave));
+  };
+
+  const saveWorking = async (value: boolean) => {
+    try {
+      await AsyncStorage.setItem(WORKING_KEY, JSON.stringify(value));
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const deleteToDo = (key: string) => {
@@ -51,6 +72,7 @@ export default function App() {
     ]);
   };
 
+  //　Appが実行した時、保存したデータを呼び出す。
   const loadToDos = async () => {
     const s: string | null = await AsyncStorage.getItem(STORGE_KEY);
     if (s !== null) {
@@ -59,6 +81,19 @@ export default function App() {
       setToDos({});
     }
   };
+
+  //　Appが実行した時、最終のデータを呼び出す。
+  const loadWorking = async () => {
+    try {
+      const savedWorking = await AsyncStorage.getItem(WORKING_KEY);
+      if (savedWorking !== null) {
+        setWorking(JSON.parse(savedWorking));
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   const addToDo = async () => {
     if (text === "") {
       return;
@@ -68,6 +103,7 @@ export default function App() {
       ...toDos,
       [Date.now()]: { text, working },
     };
+    console.log(newToDos);
     setToDos(newToDos);
     await saveToDos(newToDos);
     setText("");
